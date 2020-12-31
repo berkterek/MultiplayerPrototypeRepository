357b21967354620ebc19abf3816f917337ad3a6c
berkterek <42406263+berkterek@users.noreply.github.com>
2020-12-31 11:09:41 +0300

    Skybox Volum 2
    Skybox Volum 2 asset download from asset store and added out assetfolders

---
+++ MultiplayerPrototype/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerPrototype.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed = 10f;
+    }    
+}
