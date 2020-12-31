2ca270dc9ceaedcca46d2b9ee4dd654d9df0bd5d
berkterek <42406263+berkterek@users.noreply.github.com>
2020-12-31 12:02:55 +0300

    Floor Material Offset
    Floor material offset changes and its look like player move

---
+++ MultiplayerPrototype/Assets/GameFolders/Scripts/Concretes/Controllers/FloorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerPrototype.Controllers
+{
+    public class FloorController : MonoBehaviour
+    {
+        [SerializeField] float _moveSpeed;
+        Material _material;
+        
+        private void Awake()
+        {
+            _material = GetComponentInChildren<MeshRenderer>().material;
+        }
+
+        private void Update()
+        {
+            Vector2 direction = Vector2.down * Time.deltaTime * _moveSpeed;
+            _material.mainTextureOffset += direction;
+        }
+    }    
+}
+
